757094fd1b8ac6263edc664391d7865343a757f6
swan4er <42721152+swan4er@users.noreply.github.com>
2021-01-13 04:46:03 +0300

    Update MyProject.UE4.js

--- HTML5/MyProject.UE4.js
+++ HTML5/MyProject.UE4.js
@@ -1133,7 +1133,7 @@ $(document).ready(function() {
 				return { db: db, wasmBytes: wasmBytes, fromIndexedDB: true };
 			});
 		}).catch(function() {
-			return download(Module.locateFile('https://github.com/swan4er/test-ue4-fo-web/raw/master/HTML5/UE4Game.wasm'), 'arraybuffer').then(function(wasmBytes) {
+			return download(Module.locateFile('https://cdn.rawgit.com/swan4er/test-ue4-fo-web/raw/master/HTML5/UE4Game.wasm'), 'arraybuffer').then(function(wasmBytes) {
 				return { db: db, wasmBytes: wasmBytes, fromIndexedDB: false };
 			});
 		});
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://raw.githubusercontent.com/swan4er/test-ue4-fo-web/master/HTML5/UE4Game.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://cdn.rawgit.com/swan4er/test-ue4-fo-web/master/HTML5/UE4Game.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
@@ -1154,8 +1154,8 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://raw.githubusercontent.com/swan4er/test-ue4-fo-web/master/HTML5/MyProject.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://raw.githubusercontent.com/swan4er/test-ue4-fo-web/master/HTML5/Utility.js')).then(addScriptToDom);
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://cdn.rawgit.com/swan4er/test-ue4-fo-web/master/HTML5/MyProject.data.js'));
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://cdn.rawgit.com/swan4er/test-ue4-fo-web/master/HTML5/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('MyProject.data')).then(function(dataBlob) {
@@ -1167,9 +1167,9 @@ $(document).ready(function() {
 			});
 */
 // Instead as a fallback, download as ArrayBuffer. (TODO: Figure out the bugs with the above, and switch to using that one instead)
-			fetchOrDownloadAndStore(db, Module.locateFile('https://github.com/swan4er/test-ue4-fo-web/raw/master/HTML5/MyProject.data'), 'arraybuffer').then(function(dataArrayBuffer) {
+			fetchOrDownloadAndStore(db, Module.locateFile('https://cdn.rawgit.com/swan4er/test-ue4-fo-web/raw/master/HTML5/MyProject.data'), 'arraybuffer').then(function(dataArrayBuffer) {
 				Module['preloadedPackages'] = {};
-				Module['preloadedPackages'][Module.locateFile('https://github.com/swan4er/test-ue4-fo-web/raw/master/HTML5/MyProject.data')] = dataArrayBuffer;
+				Module['preloadedPackages'][Module.locateFile('https://cdn.rawgit.com/swan4er/test-ue4-fo-web/raw/master/HTML5/MyProject.data')] = dataArrayBuffer;
 				return dataJsDownload.then(addScriptToDom);
 			});
 
